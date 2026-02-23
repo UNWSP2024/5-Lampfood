7d8618ad2ede66db4b72b57990ddf91d6821ed6e
Elliott Morris <131560931+Lampfood@users.noreply.github.com>
2026-02-22 18:57:42 -0600

    Update program_2.py
    i wonder why it didnt commit the first time

--- program_2.py
+++ program_2.py
@@ -1,13 +1,63 @@
-# Program #2: Math Quiz
-# Write a program that gives simple math quizzes.  The program should display two random numbers to be added, such as
+#Elliott Morris, 1/29/2026, Math Quiz
+import random
 
-#     247
+def addition_quiz():
+    num1 = random.randint(1, 100)
+    num2 = random.randint(1, 100)
+    answer = int(input(f"What is {num1} + {num2}? "))
 
-# + 129
+    if answer == num1 + num2:
+        print("That is Correct")
+    else:
+        print ("That is incorrect")
+        print(f"The correct answer was {num1 + num2}")
 
-# ------
+def subtraction_quiz():
+    num1 = random.randint(1, 100)
+    num2 = random.randint(1, 100)
+    answer = int(input(f"What is {num1} - {num2}? "))
 
-# The program should allow the student to enter the answer.  
-# If the answer is correct, a message of congratulations should be displayed.  
-# If the answer is incorrect a message showing the correct answer should be displayed.  
-# The program must use a function that accomplishes part of the needed tasks.
+    if answer == num1 - num2:
+        print("That is Correct")
+    else:
+        print("That is incorrect")
+        print(f"The correct answer was {num1 - num2}")
+
+def multiplication_quiz():
+    num1 = random.randint(1, 15)
+    num2 = random.randint(1, 15)
+    answer = int(input(f"What is {num1} * {num2}? "))
+
+    if answer == num1 * num2:
+        print("That is Correct")
+    else:
+        print("That is incorrect")
+        print(f"The correct answer was {num1 * num2}")
+
+
+def division_quiz():
+    num2 = random.randint(1, 15)
+    num1 = num2 * random.randint(1, 15)
+    answer = int(input(f"What is {num1} / {num2}? "))
+
+    if answer == num1 / num2:
+        print("That is Correct")
+    else:
+        print("That is incorrect")
+        print(f"The correct answer was {num1 / num2}")
+
+def main():
+    quiz_type = input("Please enter a quiz type (Addition, Subtraction, Multiplication, Division): ").strip().lower()
+    if quiz_type == "addition":
+        addition_quiz()
+    elif quiz_type == "subtraction":
+        subtraction_quiz()
+    elif quiz_type == "multiplication":
+        multiplication_quiz()
+    elif quiz_type == "division":
+        division_quiz()
+    else:
+        print("Invalid quiz type")
+
+if __name__ == "__main__":
+    main()
